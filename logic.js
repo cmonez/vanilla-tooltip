1e738f8564382535ce14bfffecbb6bbcf80427a9
Eric Zepeda <zepedaeric@gmail.com>
2020-11-10 15:53:31 -0800

    Work on logic for getting coordinates

--- logic.js
+++ logic.js
@@ -1,2 +1,11 @@
-console.log('Hey');
-console.log('here they are', randomUsers);
+const cards = document.querySelectorAll('.user-card');
+
+cards.forEach((card) => {
+  card.addEventListener('mouseenter', () => {
+    domRect = card.getBoundingClientRect();
+    console.log('Moused over a card!');
+    console.log('Coordinates', domRect);
+  });
+});
+
+console.log('here the car', cards);
