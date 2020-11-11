78e490c281e8874952929911377a336d583a2132
Eric Zepeda <zepedaeric@gmail.com>
2020-11-10 16:26:47 -0800

    Work on adding and removing div

--- logic.js
+++ logic.js
@@ -2,10 +2,21 @@ const cards = document.querySelectorAll('.user-card');
 
 cards.forEach((card) => {
   card.addEventListener('mouseenter', () => {
+    const toolTip = document.createElement('div');
+    toolTip.classList.add('tooltip');
+    const newContent = document.createTextNode('Hi there and greetings!');
+    toolTip.appendChild(newContent);
+    card.appendChild(toolTip);
     domRect = card.getBoundingClientRect();
     console.log('Moused over a card!');
     console.log('Coordinates', domRect);
   });
 });
 
-console.log('here the car', cards);
+cards.forEach((card) => {
+  card.addEventListener('mouseleave', () => {
+    const toolTipToRemove = document.querySelector('.tooltip');
+    console.log(toolTipToRemove);
+    card.removeChild(toolTipToRemove);
+  });
+});
