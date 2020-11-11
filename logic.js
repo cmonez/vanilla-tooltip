c2a175741c10be8d1cdedcf5ea0adc8ad72b509a
Eric Zepeda <zepedaeric@gmail.com>
2020-11-10 16:41:32 -0800

    Work on grabbing dimensions for tooltip

--- logic.js
+++ logic.js
@@ -4,19 +4,23 @@ cards.forEach((card) => {
   card.addEventListener('mouseenter', () => {
     const toolTip = document.createElement('div');
     toolTip.classList.add('tooltip');
+    domRect = card.getBoundingClientRect();
+    const xCoordinate = domRect.x;
+    const topCoordinate = domRect.y;
+    const width = `${domRect.width}px`;
+    const height = `${domRect.height}px`;
+    toolTip.style.height = height;
+    toolTip.style.width = width;
     const newContent = document.createTextNode('Hi there and greetings!');
     toolTip.appendChild(newContent);
     card.appendChild(toolTip);
-    domRect = card.getBoundingClientRect();
-    console.log('Moused over a card!');
-    console.log('Coordinates', domRect);
   });
 });
 
 cards.forEach((card) => {
   card.addEventListener('mouseleave', () => {
     const toolTipToRemove = document.querySelector('.tooltip');
-    console.log(toolTipToRemove);
+    console.log('TO REMOVE', toolTipToRemove);
     card.removeChild(toolTipToRemove);
   });
 });
