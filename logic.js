3e332bd6aaa2d14409baf06320edf1cde9a7e800
Eric Zepeda <zepedaeric@gmail.com>
2020-11-10 20:43:16 -0800

    Continue refactoring tooltip logic

--- logic.js
+++ logic.js
@@ -8,7 +8,8 @@ cards.forEach((card) => {
 
 cards.forEach((card) => {
   card.addEventListener('mouseleave', function () {
-    const toolTipToRemove = document.querySelector('.tooltip');
+    // Make sure you grab the child div NOT from the whole body
+    const toolTipToRemove = card.querySelector('.tooltip');
     toolTipToRemove.style.opacity = '0';
     setTimeout(function () {
       toolTipToRemove.style.opacity = '0';
@@ -18,35 +19,24 @@ cards.forEach((card) => {
 });
 
 function createToolTip(node) {
-  if (node.lastChild.classList[0] === 'user-info') {
-    const toolTip = document.createElement('div');
-    toolTip.classList.add('tooltip');
-    domRect = node.getBoundingClientRect();
-    const xCoordinate = domRect.x;
-    const topCoordinate = domRect.y;
-    const width = domRect.width;
-    const height = domRect.height;
-    toolTip.style.height = `${height * 0.5}px`;
-    toolTip.style.width = `${width * 0.6}px`;
-    toolTip.style.marginTop = `-${height * 0.5 + 10}px`;
-    const newContent = document.createTextNode('Hi there and greetings!');
-    toolTip.appendChild(newContent);
-    node.appendChild(toolTip);
-  } else {
-    const toolTipToRemove = document.querySelector('.tooltip');
+  // If the tooltip exists already, REMOVE it
+  if (node.lastChild.classList[0] !== 'user-info') {
+    const toolTipToRemove = node.querySelector('.tooltip');
+    console.log('In tooltip remove', toolTipToRemove);
     node.removeChild(toolTipToRemove);
-    const toolTip = document.createElement('div');
-    toolTip.classList.add('tooltip');
-    domRect = node.getBoundingClientRect();
-    const xCoordinate = domRect.x;
-    const topCoordinate = domRect.y;
-    const width = domRect.width;
-    const height = domRect.height;
-    toolTip.style.height = `${height * 0.5}px`;
-    toolTip.style.width = `${width * 0.6}px`;
-    toolTip.style.marginTop = `-${height * 0.5 + 10}px`;
-    const newContent = document.createTextNode('Hi there and greetings!');
-    toolTip.appendChild(newContent);
-    node.appendChild(toolTip);
   }
+  // Set the tooltip
+  const toolTip = document.createElement('div');
+  toolTip.classList.add('tooltip');
+  domRect = node.getBoundingClientRect();
+  const xCoordinate = domRect.x;
+  const topCoordinate = domRect.y;
+  const width = domRect.width;
+  const height = domRect.height;
+  toolTip.style.height = `${height * 0.5}px`;
+  toolTip.style.width = `${width * 0.6}px`;
+  toolTip.style.marginTop = `-${height * 0.5 + 10}px`;
+  const newContent = document.createTextNode('Hi there and greetings!');
+  toolTip.appendChild(newContent);
+  node.appendChild(toolTip);
 }
