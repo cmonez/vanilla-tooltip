421bc9210a1a4d4d35415dfcf188c25981ce1583
Eric Zepeda <zepedaeric@gmail.com>
2020-11-12 00:49:33 -0800

    Reorganize logic, rename functions, add notes

--- logic.js
+++ logic.js
@@ -1,77 +1,21 @@
 const userContainer = document.querySelector('.user-container');
 
-for (let i = 0; i < randomUsers.results.length; i++) {
-  createChildDiv(randomUsers.results[i], i);
-}
+// Set cards for the first time from presaved data
+setParentContainer(randomUsers.results);
 
 const cards = document.querySelectorAll('.user-card');
 const button = document.querySelector('button');
-
 button.addEventListener('click', () => {
   fetch('https://randomuser.me/api/?results=8')
     .then((response) => response.json())
     .then((data) => {
       // Clear the userContainer
-      resetParentContainer(data.results);
+      setParentContainer(data.results);
     });
 });
 
-cards.forEach((card, index) => {
-  card.addEventListener('mouseenter', () => {
-    createToolTip(card, index, randomUsers.results[index]);
-  });
-});
-
-cards.forEach((card) => {
-  card.addEventListener('mouseleave', function () {
-    // Make sure you grab the child div NOT from the whole body
-    const toolTipToRemove = card.querySelector('.tooltip');
-    toolTipToRemove.style.opacity = '0';
-    setTimeout(function () {
-      toolTipToRemove.style.opacity = '0';
-      toolTipToRemove.remove();
-    }, 1500);
-  });
-});
-
-function createToolTip(node, index, userInformation) {
-  // If the tooltip exists already, REMOVE it
-  if (node.lastChild.classList[0] !== 'user-info') {
-    const toolTipToRemove = node.querySelector('.tooltip');
-    // console.log('In tooltip remove', toolTipToRemove);
-    node.removeChild(toolTipToRemove);
-  }
-  // Set the tooltip
-  const toolTip = document.createElement('div');
-  toolTip.classList.add('tooltip');
-  domRect = node.getBoundingClientRect();
-  const xCoordinate = domRect.x;
-  const topCoordinate = domRect.y;
-  const width = domRect.width;
-  const height = domRect.height;
-  toolTip.style.height = `${height * 0.5}px`;
-  toolTip.style.width = `${width * 0.6}px`;
-  toolTip.style.marginTop = `-${height * 0.5 + 10}px`;
-  const contactInfo = document.createElement('div');
-  const phoneNumber = document.createElement('div');
-  const email = document.createElement('div');
-
-  contactInfo.classList.add('left-spacing');
-  contactInfo.classList.add('embolden');
-  contactInfo.innerText = 'Contact Info:';
-  phoneNumber.innerText = `Phone Number: ${userInformation.cell}`;
-  email.innerText = `Email: ${userInformation.email}`;
-
-  phoneNumber.classList.add('left-spacing', 'top-spacing');
-  email.classList.add('left-spacing', 'top-spacing');
-  toolTip.appendChild(contactInfo);
-  toolTip.appendChild(phoneNumber);
-  toolTip.appendChild(email);
-
-  node.appendChild(toolTip);
-}
-
-function createChildDiv(user) {
+// Function to create card
+function createCard(user) {
   // Create user-card div and all its nested divs
   // grab information from current iterable and set
   let childDiv = document.createElement('div');
@@ -94,20 +38,70 @@ function createChildDiv(user) {
   return childDiv;
 }
 
-// Function to reset the parent container and attach events to each child
-function resetParentContainer(newCards) {
+// Function to create toolTip
+function createToolTip(node, index, userInformation) {
+  // If the tooltip exists already, REMOVE it
+  // if the lastChild for the user-card is user-info then
+  // toolTip has not been added yet (the last div)
+  if (node.lastChild.classList[0] !== 'user-info') {
+    const toolTipToRemove = node.querySelector('.tooltip');
+    // console.log('In tooltip remove', toolTipToRemove);
+    node.removeChild(toolTipToRemove);
+  }
+  // Set the tooltip
+  const toolTip = document.createElement('div');
+  toolTip.classList.add('tooltip');
+  domRect = node.getBoundingClientRect();
+  // Get coordinates of parent div to set the toolTip
+  const xCoordinate = domRect.x;
+  const topCoordinate = domRect.y;
+  const width = domRect.width;
+  const height = domRect.height;
+  toolTip.style.height = `${height * 0.5}px`;
+  toolTip.style.width = `${width * 0.6}px`;
+  // Position the toolTip on top of the card using negative margin
+  toolTip.style.marginTop = `-${height * 0.5 + 10}px`;
+  // Create child divs for the contact information of each card
+  const contactInfo = document.createElement('div');
+  const phoneNumber = document.createElement('div');
+  const email = document.createElement('div');
+  contactInfo.classList.add('left-spacing');
+  contactInfo.classList.add('embolden');
+  contactInfo.innerText = 'Contact Info:';
+  // Grab information to from userInformation to set the data dynamically for each card
+  phoneNumber.innerText = `Phone Number: ${userInformation.cell}`;
+  email.innerText = `Email: ${userInformation.email}`;
+  // Margin to toolTip divs
+  phoneNumber.classList.add('left-spacing', 'top-spacing');
+  email.classList.add('left-spacing', 'top-spacing');
+  // Append child divs to toolTip div that was created above
+  toolTip.appendChild(contactInfo);
+  toolTip.appendChild(phoneNumber);
+  toolTip.appendChild(email);
+
+  node.appendChild(toolTip);
+}
+
+// Function to set the userContainer with its child divs
+function setParentContainer(newCards) {
+  // reset userContainer
   userContainer.innerHTML = '';
-  // Iterate over results and add eventListeners for toolTip functionality
+  // Iterate over array of personObjects and add eventListeners for toolTip functionality
   for (let i = 0; i < newCards.length; i++) {
-    let createdDiv = createChildDiv(newCards[i]);
-    createdDiv.addEventListener('mouseenter', () => {
-      createToolTip(createdDiv, i, newCards[i]);
+    let createdCard = createCard(newCards[i]);
+    createdCard.addEventListener('mouseenter', () => {
+      // Create the toolTip when you enter the card
+      createToolTip(createdCard, i, newCards[i]);
     });
-    createdDiv.addEventListener('mouseleave', function () {
-      const toolTipToRemove = createdDiv.querySelector('.tooltip');
+    // Remove the toolTip when you leave the card
+    createdCard.addEventListener('mouseleave', function () {
+      // Select the toolTip within the createdDiv
+      const toolTipToRemove = createdCard.querySelector('.tooltip');
+      // Set its opacity to 0 which will occur over 1500 ms
       toolTipToRemove.style.opacity = '0';
       setTimeout(function () {
-        toolTipToRemove.style.opacity = '0';
+        // Remove the selected toolTip after 1500ms
+        // which is the same as the transition time upon mouseLeave
         toolTipToRemove.remove();
       }, 1500);
     });
